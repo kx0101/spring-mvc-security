86ce31f255949efffb7ca096bbaba30a6ce145d0
kx0101 <liakos.koulaxis@yahoo.com>
2023-05-24 19:47:40 +0300

    .

---
+++ README.md
@@ -0,0 +1,13 @@
+# Screenshots
+
+<img src="/screenshots/Screenshot from 2023-05-24 19-31-59.png" alt="Screenshot 1">
+<img src="/screenshots/Screenshot from 2023-05-24 19-33-00.png" alt="Screenshot 2">
+<img src="/screenshots/Screenshot from 2023-05-24 17-51-03.png" alt="Screenshot 3">
+<img src="/screenshots/Screenshot from 2023-05-24 17-56-17.png" alt="Screenshot 4">
+<img src="/screenshots/Screenshot from 2023-05-24 19-27-47.png" alt="Screenshot 5">
+<img src="/screenshots/Screenshot from 2023-05-24 19-28-09.png" alt="Screenshot 6">
+<img src="/screenshots/Screenshot from 2023-05-24 19-28-18.png" alt="Screenshot 7">
+<img src="/screenshots/Screenshot from 2023-05-24 19-28-32.png" alt="Screenshot 8">
+<img src="/screenshots/Screenshot from 2023-05-24 19-28-42.png" alt="Screenshot 9">
+<img src="/screenshots/Screenshot from 2023-05-24 19-28-52.png" alt="Screenshot 10">
+<img src="/screenshots/Screenshot from 2023-05-24 19-29-03.png" alt="Screenshot 11">
---
+++ src/main/java/com/elijahkx/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.elijahkx.springboot.demosecurity.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers("/").hasRole("EMPLOYEE")
+                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                .requestMatchers("/systems/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+                .formLogin(form ->
+
+                form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll())
+                .logout(logout -> logout.permitAll())
+                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+
+}
